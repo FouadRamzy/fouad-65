529129a255926e49348fe8428596bff60c5e02ca
FouadRamzy <167806569+FouadRamzy@users.noreply.github.com>
2024-04-23 00:02:34 +0200

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -1,21 +1,19 @@
-import re
+import nltk
 from collections import Counter
 from nltk.corpus import stopwords
+
 
 stop_words = set(stopwords.words('english'))
 
 with open('paragraphs.txt', 'r', encoding='utf-8') as file:
+    
     text = file.read().lower()
+    
+words = nltk.findall(r'\b\w+\b', text)
 
-# إزالة الرموز غير الأبجدية وتقسيم النص إلى كلمات
-words = re.findall(r'\b\w+\b', text)
-
-# إزالة الكلمات التوقف
 words = [word for word in words if word not in stop_words]
 
-# عد كمية كل كلمة
 word_freq = Counter(words)
 
-# عرض ترتيب تكرار الكلمات على الشاشة
 for word, freq in word_freq.items():
     print(f'{word}: {freq}')
